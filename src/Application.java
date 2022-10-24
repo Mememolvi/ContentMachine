3dc603f5f532f9ebb00365919012ad8c031947d1
neoAli <darali7575@gmail.com>
2022-10-24 15:22:36 +0530

    added basic GUI

--- src/Application.java
+++ src/Application.java
@@ -3,18 +3,20 @@ import javax.swing.*;
 public class Application extends JFrame {
 
     private JPanel mainPanel;
-    private JTextField keyWord;
     private JTextField channelName;
+    private JTextField dateToFrom;
+    private JTextField keyWord;
+    private JButton getResultButton;
+    private JTextArea linksArea;
 
-    public Application(String title)
-    {
+    public Application(String title) {
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Application application = new Application("Title");
         application.setVisible(true);
     }

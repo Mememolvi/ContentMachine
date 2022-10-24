92fbb02c27cc414996f3cc12c535afd5875503ec
neoAli <darali7575@gmail.com>
2022-10-24 16:34:58 +0530

    added hyperlink list support

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,11 @@
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Application extends JFrame {
 
@@ -7,13 +14,33 @@ public class Application extends JFrame {
     private JTextField dateToFrom;
     private JTextField keyWord;
     private JButton getResultButton;
-    private JTextArea linksArea;
+    private JEditorPane editorPane1;
 
     public Application(String title) {
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        getResultButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                editorPane1.setText("<a href=\"https://www.w3schools.com/\">Visit W3Schools.com!</a><br>\n" +
+                        "<a href=\"https://www.w3schools.com/\">Visit W3Schools.com!</a><br>" +
+                        "<a href=\"https://www.w3schools.com/\">Visit W3Schools.com!</a><br>" +
+                        "<a href=\"https://www.w3schools.com/\">Visit W3Schools.com!</a><br>" +
+                        "<a href=\"https://www.w3schools.com/\">Visit W3Schools.com!</a><br>" +
+                        "<a href=\"https://www.w3schools.com/\">Visit W3Schools.com!</a><br>" );
+            }
+        });
+        editorPane1.addHyperlinkListener(hyperlinkEvent -> {
+            if(Desktop.isDesktopSupported()){
+                try {
+                    Desktop.getDesktop().browse(hyperlinkEvent.getURL().toURI());
+                } catch (IOException | URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
